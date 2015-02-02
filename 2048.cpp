bb5afd173ed033660b5c04a9950ed6d69fd456a4
vigonz06 <victorglez96@gmail.com>
2015-02-02 17:02:16 +0100

    Update 2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -6,51 +6,52 @@
 #include <math.h>
 #include <ctime>
 #include <assert.h>
+#include <iomanip>
 
 const int DIM = 4,
-          META = 2048;
+META = 2048;
 
-const int upper_left_corner   = 218,
-          upper_right_corner  = 191,
-          lower_lenf_corner   = 192,
-          lower_right_corner  = 217,
-          carfax              = 197,
-          no_left_carfax      = 195,
-          no_right_carfax     = 180,
-          no_upper_carfax     = 194,
-          no_lower_carfax     = 193,
-          upright_line        = 179,
-          horizontal_line     = 196;
+const int upper_left_corner = 218,
+		  upper_right_corner = 191,
+		  lower_lenf_corner = 192,
+		  lower_right_corner = 217,
+		  carfax = 197,
+		  no_left_carfax = 195,
+		  no_right_carfax = 180,
+		  no_upper_carfax = 194,
+		  no_lower_carfax = 193,
+		  upright_line = 179,
+		  horizontal_line = 196;
 
-enum tDirection {left=37,up,right,down};
+enum tDirection { left = 37, up, right, down };
 
 struct tCoord
 {
-    int x; int y;
-    tCoord (int i=0, int j=0): x(i), y(j) {}
-    tCoord next(tDirection dir)
-    {
-        tCoord coor(x,y);
+	int x; int y;
+	tCoord(int i = 0, int j = 0) : x(i), y(j) {}
+	tCoord next(tDirection dir)
+	{
+		tCoord coor(x, y);
 
-        if      (dir == left  && y>0)     coor.y--;
-        else if (dir == up    && x>0)     coor.x--;
-        else if (dir == right && y<DIM-1) coor.y++;
-        else if (dir == down  && x<DIM-1) coor.x++;
+		if (dir == left  && y>0)     coor.y--;
+		else if (dir == up    && x>0)     coor.x--;
+		else if (dir == right && y<DIM - 1) coor.y++;
+		else if (dir == down  && x<DIM - 1) coor.x++;
 
-        return coor;
-    }
+		return coor;
+	}
 
-    bool operator != (tCoord coor){return x!=coor.x || y != coor.y;}
+	bool operator != (tCoord coor){ return x != coor.x || y != coor.y; }
 };
 
 struct tBoard
 {
-    int board[DIM][DIM];
+	int board[DIM][DIM];
 
-    tBoard(): board() {}
+	tBoard() : board() {}
 
-    int& operator [](const tCoord coord) {return board[coord.x][coord.y];}
-    int& operator ()(const int i, const int j) {return board[i][j];}
+	int& operator [](const tCoord coord) { return board[coord.x][coord.y]; }
+	int& operator ()(const int i, const int j) { return board[i][j]; }
 };
 
 class Listener;
@@ -61,66 +62,67 @@ class Game2048;
 class Listener
 {
 public:
-    int listen();
+	int listen();
 };
 
 class Drawer
 {
 private:
-    Game2048 *game;
+	Game2048 *game;
 public:
-    Drawer(Game2048 *g);
-        void cpConsoleOut(int cp);
-        void fontConsole();
-    	void draw();
-        void clearConsole();
-        void backgroundTextAtt(int color);
+	Drawer(Game2048 *g);
+	void cpConsoleOut(int cp);
+	void fontConsole();
+	void draw();
+	void clearConsole();
+	void backgroundTextAtt(int color);
 
-        void draw_row(int row);
-        void upper_border();
-        void lower_border();
-        void interior_border();
+	void draw_row(int row);
+	void horizontal();
+	void upper_border();
+	void lower_border();
+	void interior_border();
 };
 
 class SaveFile
 {
 private:
-    std::string file;
-    Game2048 *game;
+	std::string file;
+	Game2048 *game;
 
 public:
-    SaveFile(Game2048 *g);
-    ~SaveFile();
+	SaveFile(Game2048 *g);
+	~SaveFile();
 
-    bool save();
-    bool load();
+	bool save();
+	bool load();
 };
 
 class Game2048
 {
 private:
-    tBoard board;
-    int score;
+	tBoard board;
+	int score;
 
-    Listener listener;
-    Drawer drawer;
+	Listener listener;
+	Drawer drawer;
 	SaveFile savefile;
 
-    friend Drawer;
+	friend Drawer;
 	friend SaveFile;
 
 public:
-    Game2048();
-        void init();
-    void run();
-        void update(tDirection dir);
-            void gen_tile();
-            int max_tile();
-            bool tilt(tDirection dir);
-            bool combine_tiles(tDirection dir);
-            void getCoordMov(tDirection dir, tCoord &init, tCoord &incr);
-            bool moves_left();
-                bool is_full();
+	Game2048();
+	void init();
+	void run();
+	void update(tDirection dir);
+	void gen_tile();
+	int max_tile();
+	bool tilt(tDirection dir);
+	bool combine_tiles(tDirection dir);
+	void getCoordMov(tDirection dir, tCoord &init, tCoord &incr);
+	bool moves_left();
+	bool is_full();
 };
 
 inline int rand_int(int m);
@@ -129,37 +131,40 @@ inline int rand_int(int m);
 
 int Listener::listen()
 {
-    int key; DWORD cNumRead; INPUT_RECORD irInBuf;
-    HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
-    do{
-        ReadConsoleInput(hStdIn,&irInBuf,1,&cNumRead);
-    } while(irInBuf.EventType != KEY_EVENT);
-    if (irInBuf.Event.KeyEvent.uChar.AsciiChar == 0){
-        ReadConsoleInput(hStdIn,&irInBuf,1,&cNumRead);
-        key = irInBuf.Event.KeyEvent.wVirtualKeyCode;
-    } else key = irInBuf.Event.KeyEvent.uChar.AsciiChar;
-    return key;
+	int key; DWORD cNumRead; INPUT_RECORD irInBuf;
+	HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+	do{
+		ReadConsoleInput(hStdIn, &irInBuf, 1, &cNumRead);
+	} while (irInBuf.EventType != KEY_EVENT);
+	if (irInBuf.Event.KeyEvent.uChar.AsciiChar == 0){
+		ReadConsoleInput(hStdIn, &irInBuf, 1, &cNumRead);
+		key = irInBuf.Event.KeyEvent.wVirtualKeyCode;
+	}
+	else key = irInBuf.Event.KeyEvent.uChar.AsciiChar;
+	return key;
 }
 
-Drawer::Drawer(Game2048 *g):
-    game(g)
+Drawer::Drawer(Game2048 *g) :
+game(g)
 {
+	fontConsole();
+	cpConsoleOut(850);
 
 }
 
 void Drawer::draw()
 {
-    //system("pause");
-    clearConsole();
-    for(int i=0; i<DIM; i++)
-    {
-        for(int j=0; j<DIM; j++)
-        {
-            std::cout << game->board(i,j) << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
+	//system("pause");
+	clearConsole();
+	for (int i = 0; i<DIM; i++)
+	{
+		for (int j = 0; j<DIM; j++)
+		{
+			std::cout << game->board(i, j) << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
 }
 
 void Drawer::cpConsoleOut(int cp)
@@ -180,15 +185,114 @@ void Drawer::fontConsole()
 	SetCurrentConsoleFontEx(hStdOut, false, &cfi);
 }
 
-void Drawer::clearConsole(){system("cls");}
+void Drawer::clearConsole(){ system("cls"); }
 
 void Drawer::backgroundTextAtt(int color)
 {
-    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hStdOut, 15 | (color << 4));
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hStdOut, 15 | (color << 4));
 }
 
-SaveFile::SaveFile(Game2048 *g): game(g) {}
+void Drawer::horizontal()
+{
+	for (int i = 0; i < 7; i++)
+	{
+		std::cout << char(horizontal_line);
+	}
+}
+
+void Drawer::upper_border()
+{
+	std::cout << char(upper_left_corner);
+
+	for (int i = 0; i < DIM; i++)
+	{
+		horizontal();
+
+		if (i != (DIM-1))
+		{
+			std::cout << char(no_upper_carfax);
+		}
+		else
+		{
+			std::cout << char(upper_right_corner) << std::endl;
+		}
+	}
+}
+
+void Drawer::lower_border()
+{
+	std::cout << char(lower_lenf_corner);
+
+	for (int i = 0; i < DIM; i++)
+	{
+		horizontal();
+
+		if (i != (DIM - 1))
+		{
+			std::cout << char(no_lower_carfax);
+		}
+		else
+		{
+			std::cout << char(lower_right_corner) << std::endl;
+		}
+	}
+}
+
+void Drawer::interior_border()
+{
+	std::cout << char(no_left_carfax);
+
+	for (int i = 0; i < DIM; i++)
+	{
+		horizontal();
+
+		if (i != (DIM-1))
+		{
+			std::cout << char(carfax);
+		}
+		else
+		{
+			std::cout << char(no_right_carfax) << std::endl;
+			
+		}
+	}
+}
+
+void Drawer::draw_row(int row)
+{
+	upper_border();
+
+	for (int i = 0; i < DIM; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			std::cout << char(upright_line);
+
+			for (int k = 0; k < DIM; k++)
+			{
+				backgroundTextAtt(log2(game->board(i,k)));
+
+				std::cout << std::setw(8);
+
+				backgroundTextAtt(log2(0));
+
+				std::cout << char(upright_line);
+			}	
+			std::cout << std::endl;
+		}
+		if (i<(DIM-1))
+		{
+			interior_border();
+		}
+		else
+		{
+			lower_border();
+		}
+	}
+}
+
+SaveFile::SaveFile(Game2048 *g) : game(g) {}
 
 SaveFile::~SaveFile()
 {
@@ -198,7 +302,7 @@ bool SaveFile::save()
 {
 	char option;
 	std::cout << "Do you want to save your current game? (y/n)" << std::endl;
-    std::cin >> option;
+	std::cin >> option;
 
 	if (option = 'y')
 	{
@@ -211,182 +315,194 @@ bool SaveFile::save()
 
 bool SaveFile::load()
 {
-    std::fstream in;
-    std::cout << "Which save file do you want to load? (ENTER for New Game)" << std::endl;
-    getline(std::cin, file);
-    if (in != "")
-    {
-        in.open(file, std::fstream::in);
-        if(in.is_open())
-        {
-            //Load the board
-            for (int i=0; i<DIM; i++)
-            for (int j=0; j<DIM; j++)
-            {
-                in >> game->board(i,j);
-            }
-            in.close();
+	std::fstream in;
+	std::cout << "Which save file do you want to load? (ENTER for New Game)" << std::endl;
+	getline(std::cin, file);
+	/*if (in != "")
+	{
+		in.open(file, std::fstream::in);
+		if (in.is_open())
+		{
+			//Load the board
+			for (int i = 0; i < DIM; i++)
+				for (int j = 0; j < DIM; j++)
+				{
+					in >> game->board(i, j);
+				}
+			in.close();
 			return true;
-        }
-        else 
+		}
+		else
 		{
 			std::cout << "File not found" << std::endl;
 			return false;
 		}
-    }
-	else return false;
+	}
+	else*/ return false;
 }
 
-Game2048::Game2048():
-    score(0), drawer(this), savefile(this)
-    {
-        if(!savefile.load()) init();
-		drawer.draw();
-    }
+Game2048::Game2048() :
+score(0), drawer(this), savefile(this)
+{
+	if (!savefile.load()) init();
+	drawer.draw();
+}
 
 void Game2048::init()
 {
-    gen_tile(); gen_tile(); 
+	gen_tile(); gen_tile();
 }
 
 void Game2048::run()
 {
-    int key = up;
+	int key = up;
 
-    while(max_tile() < META && key != VK_ESCAPE && moves_left())
-    {
-        key = listener.listen();
-        update(tDirection(key));
-        drawer.draw();
-    }
+	while (max_tile() < META && key != VK_ESCAPE && moves_left())
+	{
+		key = listener.listen();
+		update(tDirection(key));
+		drawer.draw();
+	}
 }
 
 void Game2048::update(tDirection dir)
 {
-    /*The three steps must always be executed,
-          and if one changes the board, then we have to gen_tile*/
-        bool change = false;
-        change |= tilt(dir);
-        change |= combine_tiles(dir);
-        change |= tilt(dir);
-        if (change) gen_tile();
+	/*The three steps must always be executed,
+	and if one changes the board, then we have to gen_tile*/
+	bool change = false;
+	change |= tilt(dir);
+	change |= combine_tiles(dir);
+	change |= tilt(dir);
+	if (change) gen_tile();
 }
 
 void Game2048::gen_tile()
 {
-    assert(!is_full());
+	assert(!is_full());
 
-    int rx, ry;
-    do {
-    rx=rand_int(DIM); ry=rand_int(DIM);
-    }while(board(rx,ry) != 0);
+	int rx, ry;
+	do {
+		rx = rand_int(DIM); ry = rand_int(DIM);
+	} while (board(rx, ry) != 0);
 
-    board(rx, ry) = ((rand()/RAND_MAX)<0.95) ?2 :4;
+	board(rx, ry) = ((rand() / RAND_MAX)<0.95) ? 2 : 4;
 }
 
 int Game2048::max_tile()
 {
-    int mx_tile = 0;
-    for(int i=0; i<DIM; i++)
-    for(int j=0; j<DIM; j++)
-    {
-        if (board(i,j) > mx_tile) mx_tile = board(i,j);
-    }
-    return mx_tile;
+	int mx_tile = 0;
+	for (int i = 0; i<DIM; i++)
+		for (int j = 0; j<DIM; j++)
+		{
+			if (board(i, j) > mx_tile) mx_tile = board(i, j);
+		}
+	return mx_tile;
 }
 
 bool Game2048::tilt(tDirection dir)
 {
-    bool there_has_been_movement = false;
-    tCoord init, incr;
+	bool there_has_been_movement = false;
+	tCoord init, incr;
 
-    getCoordMov(dir,init,incr);
+	getCoordMov(dir, init, incr);
 
-    for(int i=init.x; 0<=i && i<DIM; i+= incr.x)
-    for(int j=init.y; 0<=j && j<DIM; j+= incr.y)
-    {
-        while(board(i,j) != 0 && board[tCoord(i,j).next(dir)] == 0)
-        {
-            board[tCoord(i,j).next(dir)] = board(i,j);
-            board(i,j) = 0;
-            i = tCoord(i,j).next(dir).x; j = tCoord(i,j).next(dir).y;
-            there_has_been_movement = true;
-        }
-    }
-    return there_has_been_movement;
+	for (int i = init.x; 0 <= i && i<DIM; i += incr.x)
+		for (int j = init.y; 0 <= j && j<DIM; j += incr.y)
+		{
+			while (board(i, j) != 0 && board[tCoord(i, j).next(dir)] == 0)
+			{
+				board[tCoord(i, j).next(dir)] = board(i, j);
+				board(i, j) = 0;
+				i = tCoord(i, j).next(dir).x; j = tCoord(i, j).next(dir).y;
+				there_has_been_movement = true;
+			}
+		}
+	return there_has_been_movement;
 }
 
 bool Game2048::combine_tiles(tDirection dir)
 {
-    tCoord init, incr; bool change = false;
-    getCoordMov(dir, init, incr);
-    for(int i=init.x; 0<=i && i<DIM; i+= incr.x)
-    for(int j=init.y; 0<=j && j<DIM; j+= incr.y)
-    {
-        if(board(i,j)!=0 && (tCoord(i,j) != tCoord(i,j).next(dir))) //Check for borders
-        {
-            if(board[tCoord(i,j).next(dir)] == board(i,j))
-            {
-                board(i,j) = 0;
-                board[tCoord(i,j).next(dir)] *= 2;
-                score += board[tCoord(i,j).next(dir)];
-                change = true;
-            }
-        }
-    }
-    return change;
+	tCoord init, incr; bool change = false;
+	getCoordMov(dir, init, incr);
+	for (int i = init.x; 0 <= i && i<DIM; i += incr.x)
+		for (int j = init.y; 0 <= j && j<DIM; j += incr.y)
+		{
+			if (board(i, j) != 0 && (tCoord(i, j) != tCoord(i, j).next(dir))) //Check for borders
+			{
+				if (board[tCoord(i, j).next(dir)] == board(i, j))
+				{
+					board(i, j) = 0;
+					board[tCoord(i, j).next(dir)] *= 2;
+					score += board[tCoord(i, j).next(dir)];
+					change = true;
+				}
+			}
+		}
+	return change;
 }
 
 void Game2048::getCoordMov(tDirection dir, tCoord &init, tCoord &incr)
 {
-    init.x = (dir == down)  ?DIM-1 :0;
-    init.y = (dir == right) ?DIM-1 :0;
-    incr.x = (dir == down)  ?-1    :1;
-    incr.y = (dir == right) ?-1    :1;
+	init.x = (dir == down) ? DIM - 1 : 0;
+	init.y = (dir == right) ? DIM - 1 : 0;
+	incr.x = (dir == down) ? -1 : 1;
+	incr.y = (dir == right) ? -1 : 1;
 }
 
 bool Game2048::moves_left()
 {
-    if(is_full())
-    {
-        bool move_found = false;
-        for (int i=left; i<=down && !move_found; i++)
-        {
-            Game2048 temp(*this);
-            if(temp.combine_tiles(tDirection(i)))
-                move_found = true;
-        }
-        return move_found;
-    }
-    else return true;
+	if (is_full())
+	{
+		bool move_found = false;
+		for (int i = left; i <= down && !move_found; i++)
+		{
+			Game2048 temp(*this);
+			if (temp.combine_tiles(tDirection(i)))
+				move_found = true;
+		}
+		return move_found;
+	}
+	else return true;
 }
 
 bool Game2048::is_full()
 {
-    bool full = true;
-    for(int i=0; i<DIM && full; i++)
-    for(int j=0; j<DIM && full; j++)
-    {
-        if (board(i,j) == 0) full = false;
-    }
-    return full;
+	bool full = true;
+	for (int i = 0; i<DIM && full; i++)
+		for (int j = 0; j<DIM && full; j++)
+		{
+			if (board(i, j) == 0) full = false;
+		}
+	return full;
 }
 
 inline int rand_int(int m)
 {
-    return rand()%m;
+	return rand() % m;
 }
 
 int log2(int x)
 {
-    int i;
-    for (i=1; pow(2,i) < x; i++);
-    return i;
+	int i;
+	for (i = 1; pow(2, i) < x; i++);
+	return i;
 }
 
 int main()
 {
-    srand(time(NULL));
-    Game2048 test;
-    test.run();
+	srand(time(NULL));
+	Game2048 test;
+	Drawer drawer(&test);
+	drawer.draw_row(0);
+	int n;
+	std::cin >> n;
+	if (n == 1)
+	{
+		std::cin.sync();
+		std::cin.get();
+	}
+	else
+	{
+		test.run();
+	}
 }
