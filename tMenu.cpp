13d1c6c0d76195f0e98d4f113df4b7f2d74fd4a5
vigonz06 <victorglez96@gmail.com>
2015-03-02 21:47:24 +0100

    Create tMenu.cpp

---
+++ tMenu.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "Game2048.h"
+#include "tMenu.h"
+
+void tMenu::menuDim()
+{
+	int otherdim;
+
+	std::cout << "4- Dimension 4x4" << std::endl
+		<< "5- Dimension 5x5" << std::endl
+		<< "6- Dimension 6x6" << std::endl
+		<< "7- Dimension 7x7" << std::endl
+		<< "8- Dimension 8x8" << std::endl;
+
+	otherdim = digitoEntre(4, 8);
+
+	game.board.changeDimension(otherdim);
+
+	game.run();
+}
+
+int tMenu::menuIni()
+{
+	std::cout << "1- Play" << std::endl
+		<< "2- Records" << std::endl
+		<< "0- Exit" << std::endl;
+
+	return digitoEntre(0,2);
+}
+
+int tMenu::menuGoal()
+{
+	std::cout << "You've reached the goal, what do you want to do?: " << std::endl
+		<< "1- Continue " << std::endl
+		<< "2- Replay " << std::endl
+		<< "0- Exit " << std::endl;
+
+	return digitoEntre(0,2);
+}
+
+void tMenu::start()
+{
+	int seleccion = menuIni();
+
+	do
+	{
+		if (seleccion == 1)
+		{
+			menuDim();
+		}
+		else if (seleccion == 2)
+		{
+			game.highscore.show();
+		}
+	} while (seleccion != 0);
+}
