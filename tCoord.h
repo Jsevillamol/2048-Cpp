66fe51bdf90f1309847b7834ebe908c6b3dcc58e
vigonz06 <victorglez96@gmail.com>
2015-03-02 21:53:01 +0100

    Create tCoord.h

---
+++ tCoord.h
@@ -0,0 +1,25 @@
+#ifndef TCOORD
+#define TCOORD
+const int DIM = 8;
+
+enum tDirection { left = 37, up, right, down };
+
+struct tCoord
+{
+	int x; int y;
+	tCoord(int i = 0, int j = 0) : x(i), y(j) {}
+	tCoord next(tDirection dir)
+	{
+		tCoord coor(x, y);
+
+		if (dir == left  && y>0)       coor.y--;
+		else if (dir == up    && x>0)       coor.x--;
+		else if (dir == right && y<DIM - 1) coor.y++;
+		else if (dir == down  && x<DIM - 1) coor.x++;
+
+		return coor;
+	}
+
+	bool operator != (tCoord coor){ return x != coor.x || y != coor.y; }
+};
+#endif
