c8365594e1d834d5845e898caf55df6a7aab1ced
vigonz06 <victorglez96@gmail.com>
2015-03-02 21:42:37 +0100

    Create Listener.cpp

---
+++ Listener.cpp
@@ -0,0 +1,21 @@
+#include "Listener.h"
+#include "winUtils.h"
+
+int Listener::listen()
+{
+	int key; DWORD cNumRead; INPUT_RECORD irInBuf;
+	HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+	do{
+		do{
+			ReadConsoleInput(hStdIn, &irInBuf, 1, &cNumRead);
+		} while (irInBuf.EventType != KEY_EVENT);
+		if (irInBuf.Event.KeyEvent.uChar.AsciiChar == 0){
+			ReadConsoleInput(hStdIn, &irInBuf, 1, &cNumRead);
+			key = irInBuf.Event.KeyEvent.wVirtualKeyCode;
+		}
+		else key = irInBuf.Event.KeyEvent.uChar.AsciiChar;
+	} while (key != VK_ESCAPE && key != VK_LEFT && key != VK_UP
+		&& key != VK_RIGHT  && key != VK_DOWN);
+	//loop until game key is pressed
+	return key;
+}
