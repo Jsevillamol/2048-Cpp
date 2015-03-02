5e8e7ef8be864559bd5374217cd309929a5a6281
vigonz06 <victorglez96@gmail.com>
2015-03-02 21:44:00 +0100

    Create SaveFile.cpp

---
+++ SaveFile.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include "SaveFile.h"
+#include "Game2048.h"
+
+bool SaveFile::save()
+{
+	char option;
+	std::cout << "Do you want to save your current game? (y/n)" << std::endl;
+	std::cin.clear();
+	std::cin >> option;
+
+	if (option == 'y')
+	{
+		std::cout << "How do you want to call your save file? (ENTER for \"" << file << "\")" << std::endl;
+		std::cin.sync();
+		std::string s;
+		getline(std::cin, s);
+		std::fstream out(((s == "") ? file : s), std::ios::out);
+		if (out.is_open())
+		{
+			for (int row = 0; row < game->board.getDim(); row++)
+			{
+				for (int col = 0; col < game->board.getDim(); col++)
+				{
+					out << game->board(row, col) << " ";
+				}
+				out << std::endl;
+			}
+			return true;
+		}
+		else
+		{
+			std::cout << "Error! The file couldn't be opened." << std::endl;
+			return false;
+		}
+	}
+	return false;
+}
+
+bool SaveFile::load()
+{
+	std::fstream in;
+	std::cout << "Which save file do you want to load? (ENTER for New Game)" << std::endl;
+	getline(std::cin, file);
+	if (file != "")
+	{
+		in.open(file, std::fstream::in);
+		if (in.is_open())
+		{
+			//Load the board
+			for (int i = 0; i < game->board.getDim(); i++)
+				for (int j = 0; j < game->board.getDim(); j++)
+				{
+					in >> game->board(i, j);
+				}
+			in.close();
+			return true;
+		}
+		else
+		{
+			std::cout << "File not found" << std::endl;
+			return false;
+		}
+	}
+	else
+	{
+		file = "savefile.txt";
+		return false;
+	}
+}
