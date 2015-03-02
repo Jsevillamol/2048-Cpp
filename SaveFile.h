0f4eec11542e9884e620e1773a1d66ad434d62f8
vigonz06 <victorglez96@gmail.com>
2015-03-02 21:52:17 +0100

    Create SaveFile.h

---
+++ SaveFile.h
@@ -0,0 +1,17 @@
+#ifndef SAVEFILE
+#define SAVEFILE
+#include <string>
+class Game2048;
+class SaveFile
+{
+private:
+	std::string file;
+	Game2048 *game;
+
+public:
+	SaveFile(Game2048 *g) : game(g) {}
+
+	bool save();
+	bool load();
+};
+#endif
