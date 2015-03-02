41773bba6174ef2a5440a37c0a2191d09b21f226
vigonz06 <victorglez96@gmail.com>
2015-03-02 21:52:40 +0100

    Create tBoard.h

---
+++ tBoard.h
@@ -0,0 +1,24 @@
+#ifndef TBOARD
+#define TBOARD
+class tBoard
+{
+private:
+	int** board;
+	int dim;
+
+public:
+	tBoard(int d = DIM);
+	~tBoard();
+
+	void create(int d);
+	void destroy();
+
+	void reset();
+	void changeDimension(const int d);
+
+	int getDim() { return dim; }
+
+	int& operator [](const tCoord coord) { return board[coord.x][coord.y]; }
+	int& operator ()(const int i, const int j) { return board[i][j]; }
+};
+#endif
