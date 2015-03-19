eac5a2ecda5c7eaea465288527f56423cc5a736c
vigonz06 <victorglez96@gmail.com>
2015-03-19 10:36:57 +0100

    añadida introduccion
    No es que no sepa ingles, pero echale un vistazo a esa intro, que puede
    haber varias catastrofes con el idioma xD

--- 2048.cpp
+++ 2048.cpp
@@ -1,9 +1,36 @@
+/*-------------------------------
+2048:
+
+Authors:
+Jaime Sevilla Molina.
+Victor Gonzalez del Hierro.
+
+Date:
+2015/3
+
+Version:
+4.0
+
+Features:
+Apart from the order in the title of the practice, we have implemented:
+1. Detection and correction of errors in data entry console.
+2. Board's size change option, between the dimensions 4, 5, 6, 8.
+3. Goal's change option, between 16, and 65536.
+4. Record's show option, depending the goal and size, from the main menu.
+5. Once you reach the goal, you can replay, conntinue the game, choosing 
+   a higher goal, or exit to the main menu.
+
+Warning:
+The way the program is implemented makes it work with exponents of 2, so that
+you do not have problems when you chose the goal. Because of that, the savefiles
+saves those exponents, not the powers.
+---------------------------------*/
+
 #include "tMenu.h"
 
 int main()
 {
-	tMenu test;
-	test.start();
-	
+	tMenu game;
+	game.start();	
 }
 
