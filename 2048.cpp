5594e52fed6870dd1b095bf201d84751cb16d752
vigonz06 <victorglez96@gmail.com>
2015-03-25 10:05:29 +0100

    corregida introduccion

--- 2048.cpp
+++ 2048.cpp
@@ -14,11 +14,11 @@ Version:
 Features:
 Apart from the order in the title of the practice, we have implemented:
 1. Detection and correction of errors in data entry console.
-2. Board's size change option, between the dimensions 4, 5, 6, 8.
-3. Goal's change option, between 16, and 65536.
-4. Record's show option, depending the goal and size, from the main menu.
-5. Once you reach the goal, you can replay, conntinue the game, choosing 
-   a higher goal, or exit to the main menu.
+2. Board size change option, between the dimensions 4, 5, 6, 8.
+3. Goal change option, between 16, and 65536.
+4. Records show option, depending the goal and size, from the main menu.
+5. Once you reach the goal, you can replay, conntinue the game (choosing 
+   a higher goal), or exit to the main menu.
 
 Warning:
 The way the program is implemented makes it work with exponents of 2, so that
